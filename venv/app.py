60b9cd565b588fef61d29061f89357aace018c8a
atepginzo <atepginzo037@gmail.com>
2025-01-08 22:33:20 +0700

    Update requirements.txt, Procfile, and runtime.txt for Railway deployment

--- venv/app.py
+++ venv/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 import os
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template('index.html')
 
